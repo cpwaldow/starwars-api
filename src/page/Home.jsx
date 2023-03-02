08838063b6e3c726109d8d37c6bac24ddfd4b841
Carlos Eduardo <cpwaldow@gmail.com>
2023-03-01 22:35:18 -0300

    criando link generico para personagens retornados por id

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,29 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Anchor = styled(Link)`
+  margin: 20px;
+  display: block;
+`;
+
+const handleLinks = () => {
+  let arr = [];
+  for (let index = 1; index <= 83; index += 1) {
+    arr.push(`/people/${index}`);
+  }
+  return arr;
+};
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      {handleLinks().map((link) => (
+        <Anchor to={link} key={link}>
+          People {link.split('/people/')[1]}
+        </Anchor>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
