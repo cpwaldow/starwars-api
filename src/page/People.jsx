11e51e9f9c581fa882e7e74601476a2091b952f3
Carlos Eduardo <cpwaldow@gmail.com>
2023-03-02 01:41:52 -0300

    removendo importação não mais utilizadas

--- src/page/People.jsx
+++ src/page/People.jsx
@@ -1,5 +1,3 @@
-import React, { useEffect, useState } from 'react';
-
 import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 import PersonName from '../components/PersonName';
