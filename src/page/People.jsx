fd057f2eb3f1dedabfb2c411fbdfa517402ce6e4
Carlos Eduardo <cpwaldow@gmail.com>
2023-03-02 01:41:04 -0300

    refatorando para aceitar os novos componentes

--- src/page/People.jsx
+++ src/page/People.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
-import api from '../services/api';
-
 import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
+import PersonName from '../components/PersonName';
 
 const Container = styled.section`
   border: 2px solid red;
@@ -15,42 +14,10 @@ const Container = styled.section`
 
 const People = () => {
   const { id } = useParams();
-  const [user, setUser] = useState();
-  const [films, setFilms] = useState();
-
-  useEffect(() => {
-    const fetchPeople = async () => {
-      const { data } = await api.get(`people/${id}`);
-      setUser(data);
-    };
-    fetchPeople();
-  }, []);
-
-  /*   useEffect(() => {
-    if (user) {
-      const movieParam = user.films.map((movie) => {
-        return movie.split('/api/')[1];
-      });
-      const fetchFilms = () => {
-        console.log('teste');
-        return movieParam.map(async (param) => {
-          console.log(param);
-          const response = await api.get(param);
-          console.log(response);
-          setFilms(response);
-        });
-      };
-      fetchFilms();
-    }
-  }, []); */
-
-  if (!user) return null;
-
   return (
     <Container>
       <section>
-        <p>{user.name}</p>
-        {/* {films && <p>{films.title}</p>} */}
+        <PersonName id={id} />
       </section>
     </Container>
   );
