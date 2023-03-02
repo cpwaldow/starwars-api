e6d40e9d81f7efff66540e692bb0ddd0b85c5bc6
Carlos Eduardo <cpwaldow@gmail.com>
2023-03-01 21:34:52 -0300

    criando rota para /home e /people

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import People from './page/People';
+import Home from './page/Home';
+import Menu from './components/Menu';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <div>CADUVERSO</div>;
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/people/:id' element={<People />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
